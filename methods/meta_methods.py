e3b4c92f7394412eac85482d5fa287d86060d7e3
DiscRoomFan36 <fletcherhogarth@gmail.com>
2024-02-21 18:54:38 +1100

    Added Append method

--- methods/meta_methods.py
+++ methods/meta_methods.py
@@ -1,10 +1,9 @@
 from method import Method, METHOD_LIST, WORKSHEET
 
-NAME = "Setup"
 class Setup(Method):
 	@property
 	def name(self):
-		return NAME
+		return "Setup"
 	
 	def display(*args, **kwargs):
 		pass
@@ -18,3 +17,18 @@ class Setup(Method):
 		args = [arg for arg in args[1:] if arg != ""]
 		for (i, arg) in enumerate(args):
 			kwargs[METHOD_LIST].insert(i, arg)
+
+class Append(Method):
+	@property
+	def name(self):
+		return "Append"
+	
+	def display(*args, **kwargs):
+		pass
+	
+	def Preform_Method(self, name: str, description: str, method: str, *args, **kwargs):
+		assert method == self.name
+
+		args = [arg for arg in args if arg != ""]
+		for arg in args:
+			kwargs[METHOD_LIST].append(arg)
